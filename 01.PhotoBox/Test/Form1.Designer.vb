f851941f9aa956decf1952346ef1c7f282a39d06
SumetPH <notsumet1@gmail.com>
2018-01-30 14:36:24 +0700

    30

---
+++ 01.PhotoBox/Test/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pb1 = New System.Windows.Forms.PictureBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"สุเมธ ผงพิลา", "ทักษิณ เพชรพรรณ", "ศรัญ สารธะวงศ์", "ณัฐวุฒิ เขียวขำ", "กิตติศักดิ์ คำใบ", "อิศรากร รัตนพระกริ่ง", "เสาวนีย์ นิยะนุช", "วิลดา กุดทิง", "ขวัญเกล้า อุปพงษ์", "สุมิตรา เรืองบุญ", "จิรประภา โชติพันธ์", "กาญจนา จันทคูณ", "ขวัญจิตร นูจันทร์", "มยุลี โยชนา", "นันทิยา วงค์สิม", "พัทริยาพร ปัญญาวุฒิ", "พลอยฤดี แก้วก่า"})
+        Me.ListBox1.Location = New System.Drawing.Point(45, 48)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(210, 500)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 577)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "รายละเอียด"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'pb1
+        '
+        Me.pb1.Location = New System.Drawing.Point(275, 12)
+        Me.pb1.Name = "pb1"
+        Me.pb1.Size = New System.Drawing.Size(700, 600)
+        Me.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pb1.TabIndex = 2
+        Me.pb1.TabStop = False
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Items.AddRange(New Object() {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17"})
+        Me.ListBox2.Location = New System.Drawing.Point(12, 48)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.ListBox2.Size = New System.Drawing.Size(27, 500)
+        Me.ListBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ISR 3 B"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(138, 577)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 35)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "ล้าง"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(989, 627)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.pb1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents pb1 As PictureBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ 01.PhotoBox/Test/Form1.vb
@@ -0,0 +1,209 @@
+﻿Public Class Form1
+    Dim s As String
+    Dim iii As String
+    Private Sub friendData(ByVal list As String)
+        If String.Equals(list, "สุเมธ ผงพิลา") Then
+            s &= "ชื่อ : สุเมธ" & vbNewLine
+            s &= "นามสกุล : ผงพิลา" & vbNewLine
+            s &= "ชื่อเล่น : น็อต" & vbNewLine
+            s &= "เพศ : ชาย" & vbNewLine
+            s &= "Facebook : facebook.com/notsumet1" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "ทักษิณ เพชรพรรณ") Then
+            s &= "ชื่อ : ทักษิณ" & vbNewLine
+            s &= "นามสกุล : เพชรพรรณ" & vbNewLine
+            s &= "ชื่อเล่น : ที" & vbNewLine
+            s &= "เพศ : ชาย" & vbNewLine
+            s &= "Facebook : facebook.com/profile.php?id=100002246414023" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "ศรัญ สารธะวงศ์") Then
+            s &= "ชื่อ : ศรัญ" & vbNewLine
+            s &= "นามสกุล : สารธะวงศ์" & vbNewLine
+            s &= "ชื่อเล่น : ยิว" & vbNewLine
+            s &= "เพศ : ชาย" & vbNewLine
+            s &= "Facebook : facebook.com/Yiw.saran" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "ณัฐวุฒิ เขียวขำ") Then
+            s &= "ชื่อ : ณัฐวุฒิ" & vbNewLine
+            s &= "นามสกุล : เขียวขำ" & vbNewLine
+            s &= "ชื่อเล่น : หมี" & vbNewLine
+            s &= "เพศ : ชาย" & vbNewLine
+            s &= "Facebook : facebook.com/nutty.nattawut" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "กิตติศักดิ์ คำใบ") Then
+            s &= "ชื่อ : กิตติศักดิ์" & vbNewLine
+            s &= "นามสกุล : คำใบ" & vbNewLine
+            s &= "ชื่อเล่น : โต้ง" & vbNewLine
+            s &= "เพศ : ชาย" & vbNewLine
+            s &= "Facebook : facebook.com/tongtang407" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "อิศรากร รัตนพระกริ่ง") Then
+            s &= "ชื่อ : อิศรากร" & vbNewLine
+            s &= "นามสกุล : รัตนพระกริ่ง" & vbNewLine
+            s &= "ชื่อเล่น : ฟลุค" & vbNewLine
+            s &= "Facebook : facebook.com/isarakorn" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "เสาวนีย์ นิยะนุช") Then
+            s &= "ชื่อ : เสาวนีย์" & vbNewLine
+            s &= "นามสกุล : นิยะนุช" & vbNewLine
+            s &= "ชื่อเล่น : โตเกียว" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/saowanee.ni" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "วิลดา กุดทิง") Then
+            s &= "ชื่อ : วิลดา" & vbNewLine
+            s &= "นามสกุล : กุดทิง" & vbNewLine
+            s &= "ชื่อเล่น : อาย" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/karasu112" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "ขวัญเกล้า อุปพงษ์") Then
+            s &= "ชื่อ : ขวัญเกล้า" & vbNewLine
+            s &= "นามสกุล : อุปพงษ์" & vbNewLine
+            s &= "ชื่อเล่น : หงส์" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/Kwunkaw.9" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "สุมิตรา เรืองบุญ") Then
+            s &= "ชื่อ : สุมิตรา" & vbNewLine
+            s &= "นามสกุล : เรืองบุญ" & vbNewLine
+            s &= "ชื่อเล่น : เจน" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/janekang.seednomnam" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "จิรประภา โชติพันธ์") Then
+            s &= "ชื่อ : จิรประภา" & vbNewLine
+            s &= "นามสกุล : โชติพันธ์" & vbNewLine
+            s &= "ชื่อเล่น : จุบแจง" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/jub.jang.716970" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "กาญจนา จันทคูณ") Then
+            s &= "ชื่อ : กาญจนา" & vbNewLine
+            s &= "นามสกุล : จันทคูณ" & vbNewLine
+            s &= "ชื่อเล่น : วาย" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/kanjana.jantacoon" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "ขวัญจิตร นูจันทร์") Then
+            s &= "ชื่อ : ขวัญจิตร" & vbNewLine
+            s &= "นามสกุล : นูจันทร์" & vbNewLine
+            s &= "ชื่อเล่น : โอ๋" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/khwanjit.nuchan.1" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "มยุลี โยชนา") Then
+            s &= "ชื่อ : มยุลี" & vbNewLine
+            s &= "นามสกุล : โยชนา" & vbNewLine
+            s &= "ชื่อเล่น : แกรน" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/gran.mayulee" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "นันทิยา วงค์สิม") Then
+            s &= "ชื่อ : นันทิยา" & vbNewLine
+            s &= "นามสกุล : วงค์สิม" & vbNewLine
+            s &= "ชื่อเล่น : กี้" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/Nunthiya.Wongsim" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "พัทริยาพร ปัญญาวุฒิ") Then
+            s &= "ชื่อ : พัทริยาพร" & vbNewLine
+            s &= "นามสกุล : ปัญญาวุฒิ" & vbNewLine
+            s &= "ชื่อเล่น : นัท" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/pattariyaporn.panyawut" & vbNewLine
+            s &= vbNewLine
+        End If
+
+        If String.Equals(list, "พลอยฤดี แก้วก่า") Then
+            s &= "ชื่อ : พลอยฤดี" & vbNewLine
+            s &= "นามสกุล : แก้วก่า" & vbNewLine
+            s &= "ชื่อเล่น : พลอย" & vbNewLine
+            s &= "เพศ : หญิง" & vbNewLine
+            s &= "Facebook : facebook.com/ployluedee.kaewka" & vbNewLine
+            s &= vbNewLine
+        End If
+
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        s = ""
+        For i = 0 To ListBox1.SelectedItems.Count - 1
+            friendData(ListBox1.SelectedItems(i))
+        Next
+        MessageBox.Show(s, "ข้อมูลประจำตัว")
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Select Case ListBox1.SelectedItem
+            Case "สุเมธ ผงพิลา"
+                pb1.Image = My.Resources.ResourceManager.GetObject("sumet")
+            Case "ทักษิณ เพชรพรรณ"
+                pb1.Image = My.Resources.ResourceManager.GetObject("t")
+            Case "ศรัญ สารธะวงศ์"
+                pb1.Image = My.Resources.ResourceManager.GetObject("yiw")
+            Case "ณัฐวุฒิ เขียวขำ"
+                pb1.Image = My.Resources.ResourceManager.GetObject("bear")
+            Case "กิตติศักดิ์ คำใบ"
+                pb1.Image = My.Resources.ResourceManager.GetObject("tong")
+            Case "อิศรากร รัตนพระกริ่ง"
+                pb1.Image = My.Resources.ResourceManager.GetObject("fuk")
+            Case "เสาวนีย์ นิยะนุช"
+                pb1.Image = My.Resources.ResourceManager.GetObject("tokyo")
+            Case "วิลดา กุดทิง"
+                pb1.Image = My.Resources.ResourceManager.GetObject("eye")
+            Case "ขวัญเกล้า อุปพงษ์"
+                pb1.Image = My.Resources.ResourceManager.GetObject("hong")
+            Case "สุมิตรา เรืองบุญ"
+                pb1.Image = My.Resources.ResourceManager.GetObject("jane")
+            Case "จิรประภา โชติพันธ์"
+                pb1.Image = My.Resources.ResourceManager.GetObject("jang")
+            Case "กาญจนา จันทคูณ"
+                pb1.Image = My.Resources.ResourceManager.GetObject("y")
+            Case "ขวัญจิตร นูจันทร์"
+                pb1.Image = My.Resources.ResourceManager.GetObject("o")
+            Case "มยุลี โยชนา"
+                pb1.Image = My.Resources.ResourceManager.GetObject("gran")
+            Case "นันทิยา วงค์สิม"
+                pb1.Image = My.Resources.ResourceManager.GetObject("kee")
+            Case "พัทริยาพร ปัญญาวุฒิ"
+                pb1.Image = My.Resources.ResourceManager.GetObject("nut")
+            Case "พลอยฤดี แก้วก่า"
+                pb1.Image = My.Resources.ResourceManager.GetObject("ploy")
+        End Select
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        pb1.Image = My.Resources.ResourceManager.GetObject("clear")
+        ListBox1.SelectedItem = Nothing
+    End Sub
+End Class
