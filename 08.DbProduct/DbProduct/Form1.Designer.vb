d186e1904d6b36bbe54fdd046d426f60a6068c17
SumetPH <notsumet1@gmail.com>
2018-02-10 17:42:16 +0700

    10.2

---
+++ 08.DbProduct/DbProduct/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextID = New System.Windows.Forms.TextBox()
+        Me.TextName = New System.Windows.Forms.TextBox()
+        Me.TextDes = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Categories : "
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 54)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(190, 329)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(246, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(26, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "รหัส"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(225, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "ชื่อหมวด"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(211, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "รายละเอียด"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(501, 65)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "รูปภาพ"
+        '
+        'TextID
+        '
+        Me.TextID.Location = New System.Drawing.Point(294, 62)
+        Me.TextID.Name = "TextID"
+        Me.TextID.ReadOnly = True
+        Me.TextID.Size = New System.Drawing.Size(117, 20)
+        Me.TextID.TabIndex = 2
+        '
+        'TextName
+        '
+        Me.TextName.Location = New System.Drawing.Point(294, 119)
+        Me.TextName.Name = "TextName"
+        Me.TextName.Size = New System.Drawing.Size(182, 20)
+        Me.TextName.TabIndex = 3
+        '
+        'TextDes
+        '
+        Me.TextDes.Location = New System.Drawing.Point(294, 183)
+        Me.TextDes.Multiline = True
+        Me.TextDes.Name = "TextDes"
+        Me.TextDes.Size = New System.Drawing.Size(182, 66)
+        Me.TextDes.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(547, 62)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(163, 187)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(651, 263)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(51, 13)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "เลือกภาพ"
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.Location = New System.Drawing.Point(294, 308)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonAdd.TabIndex = 7
+        Me.ButtonAdd.Text = "เพิ่มข้อมูล"
+        Me.ButtonAdd.UseVisualStyleBackColor = True
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Location = New System.Drawing.Point(375, 308)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonSave.TabIndex = 8
+        Me.ButtonSave.Text = "บันทึกข้อมูล"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Location = New System.Drawing.Point(456, 308)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(94, 23)
+        Me.ButtonUpdate.TabIndex = 9
+        Me.ButtonUpdate.Text = "บันทึกการแก้ไข"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Location = New System.Drawing.Point(556, 308)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonDelete.TabIndex = 10
+        Me.ButtonDelete.Text = "ลบข้อมูล"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 393)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(761, 22)
+        Me.StatusStrip1.TabIndex = 11
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(715, 17)
+        Me.ToolStripStatusLabel1.Spring = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(761, 415)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ButtonDelete)
+        Me.Controls.Add(Me.ButtonUpdate)
+        Me.Controls.Add(Me.ButtonSave)
+        Me.Controls.Add(Me.ButtonAdd)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextDes)
+        Me.Controls.Add(Me.TextName)
+        Me.Controls.Add(Me.TextID)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextID As System.Windows.Forms.TextBox
+    Friend WithEvents TextName As System.Windows.Forms.TextBox
+    Friend WithEvents TextDes As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents ButtonAdd As System.Windows.Forms.Button
+    Friend WithEvents ButtonSave As System.Windows.Forms.Button
+    Friend WithEvents ButtonUpdate As System.Windows.Forms.Button
+    Friend WithEvents ButtonDelete As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ 08.DbProduct/DbProduct/Form1.vb
@@ -0,0 +1,183 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private connecton As SqlConnection
+    Private command As SqlCommand
+    Private dataSt As DataSet
+    Private adapter As SqlDataAdapter
+    Private bindingSrc As BindingSource
+    Private sql As String
+
+    Private Sub UpdateBinding()
+        TextID.DataBindings.Clear()
+        TextName.DataBindings.Clear()
+        TextDes.DataBindings.Clear()
+        PictureBox1.DataBindings.Clear()
+
+        sql = "select * from DB_Product"
+        command = New SqlCommand(sql, connecton)
+        adapter = New SqlDataAdapter(command)
+        dataSt = New DataSet()
+        adapter.Fill(dataSt, "category")
+
+        bindingSrc = New BindingSource
+        bindingSrc.DataSource = dataSt.Tables("category")
+
+        TextID.DataBindings.Add("Text", bindingSrc, "CategoryID")
+        TextName.DataBindings.Add("Text", bindingSrc, "CategoryName")
+        TextDes.DataBindings.Add("Text", bindingSrc, "Description")
+        PictureBox1.DataBindings.Add("Image", bindingSrc, "Picture", True)
+
+        ListBox1.DataSource = dataSt.Tables("category")
+        ListBox1.DisplayMember = "CategoryName"
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim conStr As String = "Data Source=.\SQLEXPRESS;" & _
+                "AttachDbFilename=|DataDirectory|Database1.mdf;" & _
+                "Integrated Security=True;User Instance=True;"
+        connecton = New SqlConnection(conStr)
+        connecton.Open()
+        ButtonSave.Enabled = False
+        UpdateBinding()
+    End Sub
+
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        connecton.Close()
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        bindingSrc.Position = ListBox1.SelectedIndex
+        ToolStripStatusLabel1.Text = "แถวที่ : " & (bindingSrc.Position + 1) & " / " & bindingSrc.Count
+    End Sub
+
+    Private Sub ButtonAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonAdd.Click
+        TextID.Text = ""
+        TextName.Text = ""
+        TextDes.Text = ""
+        PictureBox1.Image = Nothing
+
+        If ButtonAdd.Text = "เพิ่มข้อมูล" Then
+            ButtonAdd.Text = "ยกเลิก"
+            ButtonSave.Enabled = True
+            ListBox1.Enabled = False
+        ElseIf ButtonAdd.Text = "ยกเลิก" Then
+            ButtonAdd.Text = "เพิ่มข้อมูล"
+            ButtonSave.Enabled = False
+            ListBox1.Enabled = True
+            UpdateBinding()
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        OpenFileDialog1.Filter = "ImageFile(*.jpg,*.png,*.gif,*.bmp)|*.jpg;*.png;*.gif;*.bmp"
+        If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            PictureBox1.Image = Bitmap.FromFile(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub ButtonSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonSave.Click
+        If isDataComplete() = False Then
+            Exit Sub
+        End If
+
+        sql = "insert into DB_Product"
+        If Not (PictureBox1.Image Is Nothing) Then
+            sql &= "(CategoryName, Description, Picture)"
+            sql &= "values(@name,@descr,@pic)"
+        Else
+            sql &= "(CategoryName, Description)"
+            sql &= "values(@name,@descr)"
+        End If
+
+        command.Parameters.Clear()
+        command.CommandText = sql
+        command.Parameters.AddWithValue("name", TextName.Text)
+        command.Parameters.AddWithValue("descr", TextDes.Text)
+
+        If Not (PictureBox1.Image Is Nothing) Then
+            Dim pic() As Byte = ReadImage()
+            command.Parameters.AddWithValue("pic", pic)
+        End If
+
+        Dim result As Integer = command.ExecuteNonQuery()
+        If result = -1 Then
+            MessageBox.Show("เกดข้อผิดพลาด ไม่สามารถเพิ่มข้อมูลได้")
+        Else
+            MessageBox.Show("บันทึกข้อมูลแล้ว")
+        End If
+        ButtonAdd.PerformClick()
+    End Sub
+
+    Private Function ReadImage() As Byte()
+        Dim memStream As New IO.MemoryStream()
+        PictureBox1.Image.Save(memStream, Drawing.Imaging.ImageFormat.Bmp)
+        Return memStream.ToArray()
+    End Function
+
+    Private Function IsDataComplete()
+        TextName.Text.Trim()
+        TextDes.Text.Trim()
+        If TextName.Text = "" OrElse TextDes.Text = "" Then
+            MessageBox.Show("กรุณาใส่ข้อมูลให้ครบ")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Sub ButtonUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonUpdate.Click
+        If TextID.Text = "" Or IsDataComplete() = False Then
+            Exit Sub
+        End If
+
+        Dim s As String
+        If Not (PictureBox1.Image Is Nothing) Then
+            s = ", Picture = @pic "
+        Else
+            s = " "
+        End If
+
+        sql = "update DB_Product set CategoryName = @name, Description = @descr" & s & _
+            "where CategoryID = @id"
+        command.Parameters.Clear()
+        command.CommandText = sql
+        command.Parameters.AddWithValue("name", TextName.Text)
+        command.Parameters.AddWithValue("descr", TextDes.Text)
+        command.Parameters.AddWithValue("id", TextID.Text)
+
+        If Not (PictureBox1.Image Is Nothing) Then
+            Dim byteArr() As Byte = ReadImage()
+            command.Parameters.AddWithValue("pic", byteArr)
+        End If
+
+        If command.ExecuteNonQuery() = 1 Then
+            MessageBox.Show("บันทึกการเปลี่ยนแปลงแล้ว")
+            UpdateBinding()
+        Else
+            MessageBox.Show("เกิดข้อผิดพลาด")
+        End If
+    End Sub
+
+    Private Sub ButtonDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonDelete.Click
+        If TextID.Text = "" Then
+            Exit Sub
+        End If
+
+        Dim result As DialogResult = MessageBox.Show("ท่านต้องการลบข้อมูลแถวนี้จริงหรือไม่",
+                        "ยืนยันการลบ", MessageBoxButtons.OKCancel)
+
+        If result = DialogResult.Cancel Then
+            Exit Sub
+        End If
+
+        sql = "delete from DB_Product where CategoryID = " & TextID.Text
+        command.Parameters.Clear()
+        command.CommandText = sql
+        command.ExecuteNonQuery()
+        UpdateBinding()
+    End Sub
+End Class
